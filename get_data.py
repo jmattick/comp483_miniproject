87b742547b8d1c707d57e3a474d55af21aba7a8a
Jessica Mattick <jmattick16@gmail.com>
2020-02-13 18:47:57 -0600

    add comments

--- get_data.py
+++ get_data.py
@@ -6,9 +6,9 @@ SRR = [] #list to store SRR numbers
 
 with open('acc_list.txt', 'r') as f: #open file containing accession numbers
     for line in f:
-        SRR.append(line.strip())
+        SRR.append(line.strip()) #add each accession number to SRR list
 
-base_url = 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/'
+base_url = 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/' #base url to retrieve sra files
 
 commands = [] #list to store full wget commands
 
@@ -16,4 +16,4 @@ for acc in SRR: #loop through acc numbers
     commands.append('wget -P data/ ' + base_url + str(acc) + '/' + str(acc) + '.1') #gets sra files and stores in data folder
 
 for command in commands:
-    os.system(command) #call bash command
+    os.system(command) #call bash command to retrieve each file
