affa70a23772f2e0aad35ee7dbbbcd84141d1113
Jessica Mattick <jmattick16@gmail.com>
2020-02-13 19:17:13 -0600

    get fastq files with fastq-dump

--- get_data.py
+++ get_data.py
@@ -8,12 +8,5 @@ with open('acc_list.txt', 'r') as f: #open file containing accession numbers
     for line in f:
         SRR.append(line.strip()) #add each accession number to SRR list
 
-base_url = 'https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/' #base url to retrieve sra files
-
-commands = [] #list to store full wget commands
-
 for acc in SRR: #loop through acc numbers
-    commands.append('wget -P data/ ' + base_url + str(acc) + '/' + str(acc) + '.1') #gets sra files and stores in data folder
-
-for command in commands:
-    os.system(command) #call bash command to retrieve each file
+    os.system('fastq-dump -O data/ -I --split-files ' + str(acc)) #call fastq-dump command to output fastq files in data folder
