3096b61c1cfa5a3ad2707cae994bfcb68bc91894
Jessica Mattick <jmattick16@gmail.com>
2020-02-20 15:41:01 -0600

    add sleuth_de.R

---
+++ sleuth_de.R
@@ -0,0 +1,35 @@
+#load sleuth
+library(sleuth)
+
+#sample table
+stab <- read.table("samples.txt", header=TRUE, stringsAsFactors=FALSE)
+
+#initialize object
+so <- sleuth_prep(stab)
+
+#fit a model comparing two conditions
+so <- sleuth_fit(so, ~condition, 'full')
+
+#fit reduced model to compare the likelihood ratio test
+so <- sleuth_fit(so, ~1, 'reduced')
+
+#performed the likelihood ration test for differentail expression between conditions
+so <- sleuth_lrt(so, 'reduced','full')
+
+#load dplyr
+library(dplyr)
+
+#extract the test results from the sleuth object
+sleuth_table <- sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE) 
+
+#filter most significant results (FDR/qval < 0.05)
+sleuth_significant <- dplyr::filter(sleuth_table, qval <= 0.05) %>% dplyr::arrange(pval) 
+
+#write top 10 transcripts to file
+write.table(sleuth_significant, file="sleuth_results.txt",quote = FALSE,row.names = FALSE)
+
+#extract columns for log file
+logfile_data <- select(sleuth_significant, target_id, test_stat, pval, qval)
+
+#output data to log
+write.table(logfile_data, file="miniProject.log", quote = FALSE, row.names = FALSE,  append = TRUE)
