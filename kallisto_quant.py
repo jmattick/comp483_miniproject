e1162a2222e3e5bf3dc3d8b926181ec2117b558d
Jessica Mattick <jmattick16@gmail.com>
2020-02-20 15:21:24 -0600

    add -b 30

--- kallisto_quant.py
+++ kallisto_quant.py
@@ -7,7 +7,7 @@ for file in os.listdir('data'): #loop through files in data directory
         base = file.split('_1.fastq')[0] #get basename of file
         r1 = 'data/' + str(base) + '_1.fastq' #path to read 1
         r2 = 'data/' + str(base) + '_2.fastq' #path to read 2
-        os.system('kallisto quant -i idx/genome.idx -o results/' + str(base) + ' ' + str(r1) + ' ' + str(r2)) #call kallisto quant on both paired reads        
+        os.system('kallisto quant -i idx/genome.idx -b 30 -o results/' + str(base) + ' ' + str(r1) + ' ' + str(r2)) #call kallisto quant on both paired reads        
 
 
         
