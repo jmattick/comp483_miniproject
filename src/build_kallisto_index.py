cf52921d2781fd5edbce11a85cbd8aee5913e5f7
Jessica Mattick <jmattick16@gmail.com>
2020-02-22 14:53:22 -0600

    make parameters mandatory

--- src/build_kallisto_index.py
+++ src/build_kallisto_index.py
@@ -11,6 +11,9 @@ for i in range(len(params)-1): #loop through parameters
     if params[i] == '-g' or params[i] == '--genome_dir': #if parameter is genome directory 
         genome_dir = params[i+1]
 
-os.system('kallisto index -i ' + str(genome_dir) + '/genome.idx ' + str(genome_dir) + 'genome.fa') #run command to build index from genome.fa
+if genome_dir == None:
+    print('Error: Invalid input parameters. \nSet path to directory containing genome fasta file with -g or --genome_dir.')
+else:
+    os.system('kallisto index -i ' + str(genome_dir) + '/genome.idx ' + str(genome_dir) + 'genome.fa') #run command to build index from genome.fa
 
 
