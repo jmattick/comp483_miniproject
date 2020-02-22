dfe915e5c5ccc3e0ab786bab49c380f4cf7b80e3
Jessica Mattick <jmattick16@gmail.com>
2020-02-22 14:32:28 -0600

    change build_kallisto_index.py path

--- build_kallisto_index.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-import os  #import os
-
-os.system('kallisto index -i idx/genome.idx idx/genome.fa') #run command to build index from genome.fa
-
-
---
+++ src/build_kallisto_index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import os  #import os
+import sys #import sys
+
+params = sys.argv #input parameters
+
+genome_dir = None #initialize path to genome.fastq
+
+for i in range(len(params)-1): #loop through parameters
+    if params[i] == '-g' or params[i] == '--genome_dir': #if parameter is genome directory 
+        genome_dir = params[i+1]
+
+os.system('kallisto index -i ' + str(genome_dir) + '/genome.idx ' + str(genome_dir) + 'genome.fa') #run command to build index from genome.fa
+
+
